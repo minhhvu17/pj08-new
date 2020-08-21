47a988fe8e44af85fd8929330c0539486b73e523
minhhvu17 <ngxuanminhvu17@gmail.com>
2020-08-21 14:49:35 +0700

    m

---
+++ data.js
@@ -0,0 +1,16 @@
+const DATA = [
+    {
+        ids: 1,
+        artist: "Ngọt",
+        description: "",
+        img: "",
+        song = [
+            {
+                songName : "",
+                songImage: "",
+                audio: "",
+                
+            }
+        ]
+    }
+]
---
+++ script.js
@@ -0,0 +1,42 @@
+var singer = {
+    0: {
+        artist: "Ngọt",
+        description: "Ngọt là ban nhạc indie pop với các thành viên Vũ Đinh Trọng Thắng (hát, rhythm guitar), Nguyễn Chí Hùng (lead guitar), Phan Việt Hoàng (bass guitar) và Nguyễn Hùng Nam Anh (trống). Kể từ khi được thành lập vào năm 2013 tại Hà Nội, Ngọt đã sớm tạo được tiếng vang trong cộng đồng âm nhạc underground trên toàn quốc và qua nhiều bản thu âm trên mạng xã hội, ban nhạc dần trở thành một trong những nghệ sĩ nổi bật trong dòng nhạc indie tại Việt Nam.",
+        img: "",
+        song: [
+            {
+                songName: "Cho tôi lang thang",
+                songImage: "https://photo-resize-zmp3.zadn.vn/w360_r1x1_jpeg/avatars/8/2/82c1b4ebd0ec188c2c3b0429504b8802_1507798683.jpg",
+                audio: "",
+            },
+            {
+                songName: "Cho tôi lang thang",
+                songImage: "https://photo-resize-zmp3.zadn.vn/w360_r1x1_jpeg/avatars/8/2/82c1b4ebd0ec188c2c3b0429504b8802_1507798683.jpg",
+                audio: "",
+            },
+        ]
+    }
+};
+console.log(DATA);
+
+// let content = document.getElementById('each-slider');
+// for (let i = 0; i < 5; i++) {
+//     let random = Math.floor(Math.random() * singer['0'].song.length);
+//     console.log(singer[`${i}`]);
+//     // console.log(singer['0'].song[random]);
+//     var ngheSi = singer[`${i}`].artist;
+//     var baiHat = singer[`${i}`].song[random].songName;
+//     var anhBaiHat = singer[`${i}`].song[random].songImage;
+
+//     var characterHTML = `
+//     <div class="each-slider-title">
+//         <div class="each-slider-img" style="background-image: url(${anhBaiHat})">
+//         </div>
+//         <div class="song-title">
+//             <div>${baiHat}</div>
+//             <div>${ngheSi}</div>
+//         </div>
+//     </div>
+//     `;
+//     console.log(content.insertAdjacentHTML('beforeend', characterHTML));
+// }
